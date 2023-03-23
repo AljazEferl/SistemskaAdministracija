81044555c72ee87bff330d930f100cbc26a9a222
AljazEferl <aljaz.eferl1@student.um.si>
2023-03-23 11:45:31 +0100

    Modified footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Website</title>
+    <link rel="stylesheet" href="footer_style.css">
+</head>
+
+<body>
+
+    <footer>
+        <p>&copy; Vse pravice pridržane</p>
+        <p>
+            Bližnjice <br />
+            <a class="footer-link3" href="index.php">Domov</a>
+            <a class="footer-link1" href="register.php">Registracija</a>
+            <a class="footer-link2" href="login.php">Prijava</a>
+        </p>
+    </footer>
 </body>
+
 </html>
